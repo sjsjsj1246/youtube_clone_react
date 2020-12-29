a31b10b90524daf9d9912db70e3caf0ad89a3400
Hwang In Seo <sjsjsj1246@gmail.com>
2020-12-29 23:24:35 +0900

    logo error fix

--- src/components/search_header.jsx
+++ src/components/search_header.jsx
@@ -20,7 +20,7 @@ const SearchHeader = ({ onSearch }) => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-        <img className={styles.img} src="/images/logo.png" alt="logo" />
+        <img className={styles.img} src="images/logo.png" alt="logo" />
         <h1 className={styles.title}>Youtube</h1>
       </div>
       <input
@@ -33,7 +33,7 @@ const SearchHeader = ({ onSearch }) => {
       <button className={styles.button} type="submit" onClick={onClick}>
         <img
           className={styles.buttonImg}
-          src="/images/search.png"
+          src="images/search.png"
           alt="search"
         />
       </button>
